8e477bd251ce6d547fb9a5f78c39d687eea74450
Joejoe <82959624+joejoe135791@users.noreply.github.com>
2025-01-17 12:29:50 -0800

    got 1 sha256 check done and the rest created

---
+++ py/CLI/input/sha256/checksha256hash-CLI-input.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import hashlib
+
+print("CheckHash Program by joejoe135791")
+print("Make sure that this program is executed in the same folder as the file you're checking the hash of")
+print("if you download this from the same repository and do not fully trust it. download this program from https://github.com/joejoe135791/checkhash")
+print("====================")
+while True:
+    userFilenameInput = input("Enter the file name: ")
+    filePathInput = f"{os.getcwd()}/{userFilenameInput}"
+    if os.path.isfile(filePathInput):
+        break
+    else:
+        print(f"\n{filePathInput} Not found! please try again")
+userHashInput = input("Please enter the SHA256 hash given by the repository: ")
+print(f"generating SHA256 hash of '{userFilenameInput}'")
+print(f"Your SHA256 hash:\n{userHashInput}\n")
+generatedFIleHash = hashlib.sha256(open(userFilenameInput,'rb').read()).hexdigest()
+print(f"Generated SHA256 hash of {filePathInput}:\n{generatedFIleHash}\n")
+if userHashInput == generatedFIleHash:
+    print(f"SHA256 hash verified, this program has likely not been tampered with")
+else:
+    print("THE SHA256 HASHES DO NOT MATCH UP! This program may have been modified or tampered with")
